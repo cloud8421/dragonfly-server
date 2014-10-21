16d20835f13c6ccba072e58db45a81af1d71325a
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-10-21 17:11:47 +0100

    Http adapter can cache internally.

--- lib/dragonfly_server.ex
+++ lib/dragonfly_server.ex
@@ -15,7 +15,8 @@ defmodule DragonflyServer do
 
     children = [
       :poolboy.child_spec(:dragonfly_worker_pool, worker_pool_options, []),
-      worker(ResponseCacheStore, [])
+      worker(ResponseCacheStore, []),
+      worker(HttpAdapter, [])
       # Define workers and child supervisors to be supervised
       # worker(DragonflyServer.Worker, [arg1, arg2, arg3])
     ]
--- lib/http_adapter.ex
+++ lib/http_adapter.ex
@@ -1,5 +1,36 @@
 defmodule HttpAdapter do
+  use GenServer
+  @cache_table_name :http_adapter_cache
+
   def fetch(path) do
+    GenServer.call(__MODULE__, {:fetch, path})
+  end
+
+  ## Callbacks
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_opts) do
+    :ets.new(@cache_table_name, [:named_table])
+    {:ok, []}
+  end
+
+  def handle_call({:fetch, path}, _from, state) do
+    case :ets.lookup(@cache_table_name, path) do
+      [] ->
+        data = remote_fetch(path)
+        :ets.insert(@cache_table_name, {path, data})
+        {:reply, data, state}
+      [{^path, cached_data}] ->
+        {:reply, cached_data, state}
+    end
+  end
+
+  ## Private
+
+  defp remote_fetch(path) do
     %HTTPoison.Response{body: image_binary, status_code: 200} = HTTPoison.get!(host <> "/" <> path)
     image_binary
   end
