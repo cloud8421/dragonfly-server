9d8e9e4dcc6f97f1044bb42e2de0c02024b13c0d
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-10-21 13:51:32 +0100

    Introduces adapters to fetch data.

--- lib/runner.ex
+++ lib/runner.ex
@@ -12,12 +12,16 @@ defmodule Runner do
   end
 
   defp execute(%{fetch: path, shell: transformation_command}) do
-    image_content = S3Client.fetch(path)
+    image_content = adapter.fetch(path)
     opts = [in: image_content, out: :iodata]
     result =  Porcelain.shell(transformation_command, opts)
     IO.iodata_to_binary(result.out)
   end
   defp execute(%{fetch: path}) do
-    S3Client.fetch(path)
+    adapter.fetch(path)
+  end
+
+  defp adapter do
+    Application.get_env(:storage, :adapter)
   end
 end
