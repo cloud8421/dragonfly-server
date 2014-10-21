267924609e32032e3b26883ea8f4931386bcddbb
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-10-21 11:23:25 +0100

    Adds deps and setups dotenv.

--- mix.exs
+++ mix.exs
@@ -8,26 +8,25 @@ defmodule DragonflyServer.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger],
+    [applications: app_list(Mix.env),
      mod: {DragonflyServer, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:jazz, "~> 0.2.1"}
+      {:dotenv, "~> 0.0.3"},
+      {:cowboy, "~> 1.0.0"},
+      {:plug, "~> 0.8.0"},
+      {:jazz, "~> 0.2.1"},
+      {:httpoison, "~> 0.5"},
+      {:porcelain, "~> 2.0"}
     ]
+  end
+
+  defp app_list(:dev), do: [:dotenv | app_list]
+  defp app_list(_), do: app_list
+  defp app_list do
+    [:logger, :httpoison, :porcelain, :cowboy, :plug]
   end
 end
