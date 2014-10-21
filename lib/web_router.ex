41f3b433ec4037cf5b2b9adc0f1053100768b052
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-10-21 17:12:21 +0100

    Small refactor to extract image computation logic.

--- lib/web_router.ex
+++ lib/web_router.ex
@@ -2,17 +2,23 @@ defmodule WebRouter do
   import Plug.Conn
   use Plug.Router
 
+  # Plug order matters, as they are inserted as middlewares
+  plug Plug.Head
   plug :match
   plug :dispatch
 
   get "/media/:payload/:filename" do
-    processed = :poolboy.transaction(:dragonfly_worker_pool, fn(worker) ->
-      JobWorker.process(worker, payload)
-    end)
-    send_resp(conn, 200, processed)
+    response = compute_image(payload)
+    send_resp(conn, 200, response)
   end
 
   match _ do
     send_resp(conn, 404, "Image not found")
+  end
+
+  defp compute_image(payload) do
+    :poolboy.transaction(:dragonfly_worker_pool, fn(worker) ->
+      JobWorker.process(worker, payload)
+    end)
   end
 end
