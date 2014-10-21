bd019cfbdff18ff42f7ff20ab8025ba3d30bb40b
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-10-21 12:02:10 +0100

    Adds super simple S3 client.

---
+++ lib/s3_client.ex
@@ -0,0 +1,10 @@
+defmodule S3Client do
+  def fetch(path) do
+    %HTTPoison.Response{body: image_binary, status_code: 200} = HTTPoison.get!(host <> "/" <> path)
+    image_binary
+  end
+
+  defp host do
+    System.get_env("S3_BASE_URL")
+  end
+end
