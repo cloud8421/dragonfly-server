a7990e90882d2f7cbb94594b5fac23302b8ef236
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-10-22 10:52:53 +0100

    Deleting a job deletes all associated resources.
    Refactors some of job plumbing to isolate responsibilities.

--- lib/web_router.ex
+++ lib/web_router.ex
@@ -9,16 +9,13 @@ defmodule WebRouter do
   plug :dispatch
 
   delete "/:payload" do
-    JobCacheStore.delete(payload)
+    :ok = expire_image(payload)
     send_resp(conn, 202, "Scheduled deletion")
   end
 
   get "/media/:payload/:filename" do
     {format, response} = case JobCacheStore.get(payload) do
-      nil ->
-        {format, data} = compute_image(payload)
-        JobCacheStore.set(payload, format, data)
-        {format, data}
+      nil -> compute_image(payload)
       match -> match
     end
     conn_with_headers = add_headers(conn, format, filename)
@@ -32,6 +29,12 @@ defmodule WebRouter do
   defp compute_image(payload) do
     :poolboy.transaction(:dragonfly_worker_pool, fn(worker) ->
       JobWorker.process(worker, payload)
+    end)
+  end
+
+  def expire_image(payload) do
+    :poolboy.transaction(:dragonfly_worker_pool, fn(worker) ->
+      JobWorker.expire(worker, payload)
     end)
   end
 
