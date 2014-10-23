0e51f044455ed980e65ab421e8671c4ec3efc20e
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-10-23 15:12:54 +0100

    Fs engine fix for abs_path computation.

--- lib/engines/fs_engine.ex
+++ lib/engines/fs_engine.ex
@@ -1,13 +1,11 @@
 defmodule FsEngine do
-  def fetch(path), do: read_file(path)
-
-  defp read_file(path = "/" <> _rest) do
-    {:ok, image_binary} = File.read(path)
-  end
-  defp read_file(path) do
-    {:ok, image_binary} = File.read(base_path <> "/" <> path)
+  def fetch(path) do
+    {:ok, image_binary} = abs_path(path) |> File.read
     image_binary
   end
+
+  defp abs_path(path = "/" <> _rest), do: path
+  defp abs_path(path), do: base_path <> "/" <> path
 
   defp base_path do
     Application.get_env(:storage, :base_path)
