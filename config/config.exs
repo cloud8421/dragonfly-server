99deb354baaa9b4ed00e684d52d1921c17464057
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-12-04 17:50:14 +0000

    Explicitly reference all config options.

--- config/config.exs
+++ config/config.exs
@@ -4,9 +4,21 @@ use Mix.Config
 
 config :processor,
   job_worker_timeout: 15000,
-  http_fetch_timeout: 10000
+  job_worker_pool_size: 50,
+  http_fetch_timeout: 10000,
+  convert_command: "convert"
 
 config :cache,
   store: :memcached
+
+# config :web_server,
+#   acceptors: 50
+#
+# config :security,
+#   verify_urls: true,
+#   secret: "my-secret"
+#
+# config :storage,
+#   base_path: "./"
 
 import_config "#{Mix.env}.exs"
