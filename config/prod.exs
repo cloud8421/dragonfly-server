8238b2b4bd6b4d11f8f272936eca23e8913d5578
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-03-16 13:23:25 +0000

    Do not use Erlang's error_logger in prod.

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,10 @@
 use Mix.Config
 
 config :logger, :console,
-  level: :info, metadata: [:request_id]
+  level: :info,
+  metadata: [:request_id],
+  handle_otp_reports: true,
+  handle_sasl_reports: true
 
 config :cache,
   store: :memcached
