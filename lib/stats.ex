7592144132cdf2f6a0c7f01105f151382c8fa40e
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-03-16 22:04:06 +0000

    Revert "Adds memory usage stats to /stats."
    This reverts commit 886e2886a253b30051d15e31e81ec75fdbdf2373.

--- lib/stats.ex
+++ lib/stats.ex
@@ -5,8 +5,7 @@ defmodule Stats do
       available_workers: GenServer.call(:dragonfly_worker_pool, :get_avail_workers)
                          |> Enum.count,
       bin_memory_usage_in_kb: stats |> extract_binary_memory_usage,
-      total_memory_usage_in_kb: stats |> extract_total_memory_usage,
-      procs_by_memory: procs_by_memory
+      total_memory_usage_in_kb: stats |> extract_total_memory_usage
     }
   end
 
@@ -24,14 +23,4 @@ defmodule Stats do
     [{data, _discard}] = stats
     data[:memory_total] |> div 1024
   end
-
-  defp procs_by_memory do
-    procs = :recon.proc_count(:binary_memory, 10)
-    Enum.map(procs, fn({_pid, memory, data}) ->
-      {data |> format_memory_info, memory |> div 1024}
-    end)
-  end
-
-  defp format_memory_info([name, _func, _initial]), do: name
-  defp format_memory_info([_func, _initial]), do: "anonymous_function"
 end
